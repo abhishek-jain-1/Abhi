7a6ba6104f081d48310e44c33ada18da0de042ce
abhishek-jain-1 <abhij1904@gmail.com>
2019-09-24 15:57:48 +0530

    calculator

---
+++ Simple Calculator.py
@@ -0,0 +1,20 @@
+a = int(input("Number 1"))
+b = int(input("Number 2"))
+add = a+b
+sub = a-b
+mul = a*b
+div = a/b
+mod = a%b
+exp = a**b
+flo = a//b
+cuberoot = a**(1/3)
+squareroot = a**(1/2)
+print (add)
+print (sub)
+print (mul)
+print (div)
+print (mod)
+print (exp)
+print (flo)
+print (cuberoot)
+print (squareroot)
