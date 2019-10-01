44c73257ed11ce772e6ae4868f0b9a88fb5785a5
abhishek-jain-1 <abhij1904@gmail.com>
2019-10-01 15:38:44 +0530

    mark

---
+++ mark.py
@@ -0,0 +1,53 @@
+a = float(input("Sub 1"))
+b = float(input("Sub 2"))
+c = float(input("Sub 3"))
+d = float(input("Sub 4"))
+e = float(input("Sub 5"))
+
+if(a>90):
+    print("Grade A")
+elif(a>80):
+    print("Grade B")
+elif(a>70):
+    print("Grade C")
+else:
+    print("TOO BAD YOU FAIL!!!!")
+    
+
+if(b>90):
+    print("Grade A")
+elif(b>80):
+    print("Grade B")
+elif(b>70):
+    print("Grade C")
+else:
+    print("TOO BAD YOU FAIL!!!!")
+
+if(c>90):
+    print("Grade A")
+elif(c>80):
+    print("Grade B")
+elif(c>70):
+    print("Grade C")
+else:
+    print("TOO BAD YOU FAIL!!!!")
+
+
+if(d>90):
+    print("Grade A")
+elif(d>80):
+    print("Grade B")
+elif(d>70):
+    print("Grade C")
+else:
+    print("TOO BAD YOU FAIL!!!!")
+
+if(e>90):
+    print("Grade A")
+elif(e>80):
+    print("Grade B")
+elif(e>70):
+    print("Grade C")
+else:
+    print("TOO BAD YOU FAIL!!!!")
+    
